fc0e15631ba744db25c16c810496f7e7323390c8
Gilles Dartiguelongue <eva@gentoo.org>
2015-04-19 20:45:55 +0200

    Add tool to extract translations from gtk+

---
+++ utils/extract-po.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys 
+
+
+def extract_translations():
+    """ Extract translation for updateiconcache. """
+
+    with open(sys.argv[1]) as orig_po, \
+         open(sys.argv[2], 'w+') as new_po:
+
+        lines = []
+        relevant = True
+
+        for line in orig_po.readlines():
+            lines.append(line)
+
+            if line.strip() == '':
+                if relevant:
+                    relevant = False
+                    for wline in lines:
+                        new_po.write(wline)
+                lines = []
+                continue
+
+            if line.startswith('#:'):
+                if './gtk/updateiconcache.c' in line:
+                    relevant = True
+
+        if relevant:
+            for wline in lines:
+                new_po.write(wline)
+
+
+if __name__ == '__main__':
+    extract_translations()
